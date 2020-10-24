c69a609910c739272cc05a246e628a1865798059
Angela <67710978+angelaeang@users.noreply.github.com>
2020-10-24 14:53:39 -0400

    Initial Commit

---
+++ TipPal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipPal
+//
+//  Created by Angela Eang on 10/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
